7b258ca8c131e354f595b5c47fe5dbee5e00106f
Nikzeal <lacarn@usi.ch>
2025-11-19 18:23:06 +0100

    Modify start function for iucn, check for api

--- wildlife/wildlife/spiders/iucn_spider.py
+++ wildlife/wildlife/spiders/iucn_spider.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import scrapy
 import json
 
@@ -13,20 +14,31 @@ class IucnSpider(scrapy.Spider):
     # species_list = ["panthera_tigris", "elephas_maximus", "gorilla_gorilla"]
 
     async def start(self):
-        for sp in self.species_list:
-            url = f"https://apiv3.iucnredlist.org/api/v3/species/{sp}?token={API_TOKEN}"
-            yield scrapy.Request(url, callback=self.parse)
+        start_urls = [
+            "https://www.iucnredlist.org/search?searchType=species"]
+        
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+        # for sp in self.species_list:
+        #     url = f"https://apiv3.iucnredlist.org/api/v3/species/{sp}?token={API_TOKEN}"
+        #     yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        data = json.loads(response.text)
-        result = data.get("result", [{}])[0]
+        page = response.url.split("/")[-2]
+        filename = f"wwf-{page}.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
 
-        yield {
-            "source": "IUCN",
-            "scientific_name": result.get("scientific_name"),
-            "category": result.get("category"),
-            "population_trend": result.get("population_trend"),
-            "habitat": result.get("habitat"),
-            "threats": result.get("threats"),
-            "meta": result,
-        }
+        # data = json.loads(response.text)
+        # result = data.get("result", [{}])[0]
+
+        # yield {
+        #     "source": "IUCN",
+        #     "scientific_name": result.get("scientific_name"),
+        #     "category": result.get("category"),
+        #     "population_trend": result.get("population_trend"),
+        #     "habitat": result.get("habitat"),
+        #     "threats": result.get("threats"),
+        #     "meta": result,
+        # }
