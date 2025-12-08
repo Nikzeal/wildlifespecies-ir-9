abead58907905cd758362f2e0110e6a242b1f354
Nikzeal <lacarn@usi.ch>
2025-12-08 17:41:14 +0100

    Fix python script for search

--- wildlife/frontend/search_cli.py
+++ wildlife/frontend/search_cli.py
@@ -1,5 +1,7 @@
 import requests
 from pyscript import document, when
+from js import console
+
 
 
 SOLR_URL = "http://localhost:8983/solr/wild_life/select"
@@ -7,6 +9,7 @@ SOLR_URL = "http://localhost:8983/solr/wild_life/select"
 
 
 def search(query, rows=10):
+   
     params = {
         "q": f"{query}~2",
         "defType": "edismax",
@@ -21,43 +24,35 @@ def search(query, rows=10):
 
 
 
-searchbar = document.querySelector("#search")
 
-@when(searchbar, "search")
-def on_search(event):
+searchbar = document.querySelector("#search")
+searchbtn = document.querySelector("#search-btn")
+
+
+@when("click", searchbtn)
+def on_search_click(event):
+    
     query = searchbar.value
+    console.log(f"Searching for: {query}")
+
     results = search(query)
     resultsContainer = document.querySelector("#results")
     resultsContainer.innerHTML = ""
     for r in results:
         item = document.createElement("div")
-        item.className = "result-item"
+        item.className = "item"
         item.innerHTML = f"""
-            <h3><a href="{r.get('url')}" target="_blank">{r.get('name')}</a></h3>
-            <p><em>{r.get('scientific_name')}</em></p>
+            <img src="{r.get('image_url')}" alt="{r.get('name')}"  />
+            <div class="text">
+
+
+                <a href="{r.get('url')}" target="_blank">{r.get('name')} - {r.get('scientific_name')}</a>
+                <p class="overview">{r.get('summary')}</p>
+
+            </div>
         """
+
         resultsContainer.appendChild(item)
 
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    while True:
-        q = input("Query: ")
-        if not q:
-            break
-
-        results = search(q)
-        print()
-        for r in results:
-            print("•", r.get("name"), "-", r.get("scientific_name"))
-            print(" ", r.get("url"))
-        print()
 
 
